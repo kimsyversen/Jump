238f6148c811f4b31dfdccd2b1f53194b56672bd
kentarneb <kent_arne@hotmail.com>
2014-03-03 14:20:42 +0100

    Add: CollisionDetection, bug

--- WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Floors/IFloor.cs
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Floors/IFloor.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Floors
 {
     interface IFloor : ISprite
     {
-        void Blabla();
+         Rectangle Collide { get; }
     }
 }
--- WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Player/Concretes/PlayerNotFontSprite.cs
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Player/Concretes/PlayerNotFontSprite.cs
@@ -12,8 +12,8 @@ namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Player.Concretes
     {
         private Game _game;
         private readonly List<IFont> _observers;
-        bool hasJumped;
-        bool hasHitTheWall;
+        private bool _hasJumped;
+        private bool _hasHitTheWall;
 
         public PlayerNotFontSprite(Game game) : this(game.Content.Load<Texture2D>(@"Ball"), 
                 new Vector2(game.Window.ClientBounds.Width / 2f, game.Window.ClientBounds.Height / 2f), new Point(30, 30), new Point(0, 0),
@@ -21,8 +21,8 @@ namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Player.Concretes
         {
             _game = game;
             _observers = new List<IFont>();
-            hasJumped = true;
-            hasHitTheWall = false;
+            _hasJumped = true;
+            _hasHitTheWall = false;
         }
 
 
@@ -50,45 +50,45 @@ namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Player.Concretes
         {
 
 
-            if (Keyboard.GetState().IsKeyDown(Keys.D) && hasHitTheWall == false) Speed.X = playerSpeed;
-            else if (Keyboard.GetState().IsKeyDown(Keys.A) && hasHitTheWall == false) Speed.X = -playerSpeed; else if (hasHitTheWall == false) Speed.X = 0f;
+            if (Keyboard.GetState().IsKeyDown(Keys.D) && _hasHitTheWall == false) Speed.X = playerSpeed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.A) && _hasHitTheWall == false) Speed.X = -playerSpeed; else if (_hasHitTheWall == false) Speed.X = 0f;
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Space) && hasJumped == false)
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && _hasJumped == false)
             {
                 SpritePosition.Y -= 10f;
                 Speed.Y = -20f;
-                hasJumped = true;
+                _hasJumped = true;
             }
-            if (hasJumped == true)
+            if (_hasJumped == true)
             {
                 float i = 1;
                 Speed.Y += 0.15f + i;
             }
             if (SpritePosition.Y + Texture.Height > clientBounds.Height)
             {
-                hasJumped = false;
-                hasHitTheWall = false;
+                _hasJumped = false;
+                _hasHitTheWall = false;
             }
 
-            if (hasJumped == false)
+            if (_hasJumped == false)
                 Speed.Y = 0f;
 
             if (SpritePosition.X <= 0)
             {
-                if(hasJumped == true){
+                if(_hasJumped == true){
                     Speed.X = playerSpeed;
                     Speed.Y = -playerSpeed - 5;
-                    hasHitTheWall = true;
+                    _hasHitTheWall = true;
                 }else SpritePosition.X =0;
                 
             }
             if (SpritePosition.X >= (clientBounds.Width - Texture.Width))
             {
-                if (hasJumped == true)
+                if (_hasJumped == true)
                 {
                     Speed.X = -playerSpeed;
                     Speed.Y = -playerSpeed - 5;
-                    hasHitTheWall = true;
+                    _hasHitTheWall = true;
                 }
                 else SpritePosition.X = clientBounds.Width - Texture.Width;   
             }
@@ -120,13 +120,11 @@ namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Player.Concretes
             get { return SpritePosition; }
             set { SpritePosition = value; }
         }
-
+        public Rectangle Collide { get { return CollisionRectangle; } private set; }
         #region ObserverPatternRelated
 
-        public void UpdateBlabla()
-        {
-            throw new System.NotImplementedException();
-        }
+ 
+     
 
         public void RegisterObserver(IFont observer)
         {
