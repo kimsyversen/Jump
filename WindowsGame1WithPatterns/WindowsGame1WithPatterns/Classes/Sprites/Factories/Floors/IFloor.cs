6fa8cf77743148c30f52440e62babbf6b8327069
kentarneb <kent_arne@hotmail.com>
2014-03-03 16:01:19 +0100

    Add: Working

--- WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Floors/IFloor.cs
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Sprites/Factories/Floors/IFloor.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace WindowsGame1WithPatterns.Classes.Sprites.Factories.Floors
 {
-    interface IFloor : ISprite
+    internal interface IFloor : ISprite
     {
-         Rectangle Collide { get; }
-         Vector2 FloorPosition { get; set; }
-         Texture2D FloorTexture { get; set; }
+        Rectangle Collide { get; }
+        Vector2 FloorPosition { get; set; }
+        Texture2D FloorTexture { get; set; }
+
 
     }
 }
