bba2b03f507e9135a1272dc77f7cb23dea8b8324
HoffernJr <per.stian.hoff@gmail.com>
2014-03-10 12:03:12 +0100

    Camera class added

---
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/CameraConfiguration/CameraManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1WithPatterns.Classes.CameraConfiguration
+{
+    class CameraManager
+    {
+        private Matrix transform;
+        public Matrix Transform
+        {
+            get { return transform; }
+        }
+        private Vector2 center;
+        private Viewport viewport;
+
+        public CameraManager(Viewport newViewport)
+        {
+            viewport = newViewport;
+        }
+        public void Update(Vector2 position, int xOffset, int yOffset) 
+        {
+            if (position.X < viewport.Width / 2)
+                center.X = viewport.Width / 2;
+            else if (position.X > xOffset - (viewport.Width / 2))
+                center.X = xOffset - (viewport.Width / 2);
+            else center.X = position.X;
+
+            if (position.Y < viewport.Height / 2)
+                center.Y = viewport.Height / 2;
+            else if (position.Y > yOffset - (viewport.Height / 2))
+                center.Y = yOffset - (viewport.Height / 2);
+            else center.Y = position.Y;
+
+            transform = Matrix.CreateTranslation(new Vector3(-center.X + (viewport.Width / 2),
+                -center.Y + (viewport.Height / 2), 0));
+        }
+
+    }
+}
