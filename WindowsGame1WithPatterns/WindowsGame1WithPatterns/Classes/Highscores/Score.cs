e0aa055db0587977cc16807689b59ed1344f589a
Kim Syversen <kim.syversen@gmail.com>
2014-03-17 22:04:34 +0100

    Add: Comments

--- WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Highscores/Score.cs
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/Highscores/Score.cs
@@ -5,9 +5,19 @@ using System.Text;
 
 namespace WindowsGame1WithPatterns.Classes.Highscores
 {
+    /// <summary>
+    /// Used to set/get points and name for a player
+    /// </summary>
     class Score
     {
+        /// <summary>
+        /// Get or set points
+        /// </summary>
         public int Points { get; set; }
+
+        /// <summary>
+        /// Get or set name
+        /// </summary>
         public string Name { get; set; }
 
         public Score(int newPoints, string newName)
