052cc773dcc9966bbb4ebfc3963c85214e1911e8
Kim Syversen <kim.syversen@gmail.om>
2014-03-18 12:52:33 +0100

    Change: Removed TODO

--- WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/CameraConfiguration/CameraManager.cs
+++ WindowsGame1WithPatterns/WindowsGame1WithPatterns/Classes/CameraConfiguration/CameraManager.cs
@@ -93,7 +93,6 @@ namespace WindowsGame1WithPatterns.Classes.CameraConfiguration
             _increaseSpeed += VelocityDelta;
         }
 
-        //TODO: Bryr seg bare om første spiller. Spill multiplayer og prøv en og en spiller. Se forskjell
         /// <summary>
         /// Updates the camera position depending on where the player is located.
         /// </summary>
